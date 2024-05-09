8843991f7126d65852db241647e12646ed35554b
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-05-09 12:55:00 +0200

    Fixed data_prep.py

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,44 +1,23 @@
-#data preprocessing
-
+# data_preprocessing.py
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import pickle
+import yaml
 import os
 from joblib import dump
 
-# tokenizing
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from sklearn.preprocessing import LabelEncoder
+
+def load_data(file_path):
+    data = [line.strip() for line in open(file_path, "r").readlines()]
+    x_data = [line.split("\t")[1] for line in data]
+    y_data = [line.split("\t")[0] for line in data]
+    return x_data, y_data
 
 
-def split_dataset(dataset_folder="../../data/raw/DL Dataset/", train_file="train.txt",
-                  test_file="test.txt", val_file="val.txt"):
-
-    with open(dataset_folder + train_file, "r", encoding="utf-8") as train_lines:
-        train = [line.strip() for line in train_lines.readlines()]
-        raw_x_train = [line.split("\t")[1] for line in train]
-        raw_y_train = [line.split("\t")[0] for line in train]
-
-    with open(dataset_folder + test_file, "r", encoding="utf-8") as test_lines:
-        test = [line.strip() for line in test_lines.readlines()]
-        raw_x_test = [line.split("\t")[1] for line in test]
-        raw_y_test = [line.split("\t")[0] for line in test]
-
-    with open(dataset_folder + val_file, "r", encoding="utf-8") as val_lines:
-        val = [line.strip() for line in val_lines.readlines()]
-        raw_x_val = [line.split("\t")[1] for line in val]
-        raw_y_val = [line.split("\t")[0] for line in val]
-
-    print("Train size: ", len(raw_x_train))
-    print("Test size: ", len(raw_x_test))
-    print("Val size: ", len(raw_x_val))
-
-    return [raw_x_train, raw_x_test, raw_x_val], [raw_y_train, raw_y_test, raw_y_val]
-
-
-def tokenizing(raw_x, raw_y):
-    """Tokenizes the raw data."""
-    raw_x_train, raw_x_test, raw_x_val = raw_x
-    raw_y_train, raw_y_test, raw_y_val = raw_y
-
+def preprocess_data(raw_x_train, raw_y_train, raw_x_val, raw_y_val, raw_x_test, raw_y_test):
+    # Tokenization and Padding
     tokenizer = Tokenizer(lower=True, char_level=True, oov_token='-n-')
     tokenizer.fit_on_texts(raw_x_train + raw_x_val + raw_x_test)
     char_index = tokenizer.word_index
@@ -47,34 +26,46 @@ def tokenizing(raw_x, raw_y):
     x_val = pad_sequences(tokenizer.texts_to_sequences(raw_x_val), maxlen=sequence_length)
     x_test = pad_sequences(tokenizer.texts_to_sequences(raw_x_test), maxlen=sequence_length)
 
+    # Encoding Labels
     encoder = LabelEncoder()
-
     y_train = encoder.fit_transform(raw_y_train)
     y_val = encoder.transform(raw_y_val)
     y_test = encoder.transform(raw_y_test)
 
-    return char_index, [x_train, x_val, x_test], [y_train, y_val, y_test]
+    return [x_train, y_train], [x_val, y_val], [x_test, y_test], char_index
 
 
-# main function
 def main():
+    with open("configs/params.yaml") as stream:
+        try:
+            params = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            raise "Could not load params.yaml"
 
-    dataset_folder = "../../data/raw/DL Dataset/"
+    data_path = params['dataset_dir'] + 'raw_data/DL Dataset/'
 
-    # check if there is data inside the folder
-    if not os.path.exists(dataset_folder):
-        raise FileNotFoundError(f"Dataset folder '{dataset_folder}' is empty")
+    train_file = data_path + "train.txt"
+    val_file = data_path + "val.txt"
+    test_file = data_path + "test.txt"
 
-    raw_x, raw_y = split_dataset()
-    char_index, tokenized_x, tokenized_y = tokenizing(raw_x, raw_y)
+    raw_x_train, raw_y_train = load_data(train_file)
+    raw_x_val, raw_y_val = load_data(val_file)
+    raw_x_test, raw_y_test = load_data(test_file)
 
-    output_folder = "../../data/processed"
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+    ds_train, ds_val, ds_test, char_index = preprocess_data(raw_x_train, raw_y_train, raw_x_val, raw_y_val, raw_x_test, raw_y_test)
 
-    dump(char_index, f'{output_folder}/char_index.joblib')
-    dump(tokenized_x, f'{output_folder}/x_data.joblib')
-    dump(tokenized_y, f'{output_folder}/y_data.joblib')
+    out_path = params['dataset_dir'] + 'processed_data/'
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
 
-if __name__ == "__main__":
+    dump(ds_train, f'{out_path}ds_train.joblib')
+    dump(ds_val, f'{out_path}ds_val.joblib')
+    dump(ds_test, f'{out_path}ds_test.joblib')
+    dump(char_index, f'{out_path}char_index.joblib')
+
+    print("Done preprocessing data. Exiting data_prep.py")
+
+
+if __name__ == '__main__':
     main()
