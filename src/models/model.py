24cf225864e56a60460cf156db8b48f2cdb6ad37
Mitali Patil <m.s.patil@student.tudelft.nl>
2024-06-27 12:03:45 +0200

    model v2

--- src/models/model.py
+++ src/models/model.py
@@ -18,28 +18,28 @@ def create_model(voc_size, categories):
     model = Sequential()
     model.add(Embedding(voc_size + 1, 50))
 
-    model.add(Conv1D(128, 3, activation='tanh'))
+    model.add(Conv1D(128, 3, activation='relu'))
     model.add(MaxPooling1D(3))
     model.add(Dropout(0.2))
 
-    model.add(Conv1D(128, 7, activation='tanh', padding='same'))
+    model.add(Conv1D(128, 7, activation='relu', padding='same'))
     model.add(Dropout(0.2))
 
-    model.add(Conv1D(128, 5, activation='tanh', padding='same'))
+    model.add(Conv1D(128, 5, activation='relu', padding='same'))
     model.add(Dropout(0.2))
 
-    model.add(Conv1D(128, 3, activation='tanh', padding='same'))
+    model.add(Conv1D(128, 3, activation='relu', padding='same'))
     model.add(MaxPooling1D(3))
     model.add(Dropout(0.2))
 
-    model.add(Conv1D(128, 5, activation='tanh', padding='same'))
+    model.add(Conv1D(128, 5, activation='relu', padding='same'))
     model.add(Dropout(0.2))
 
-    model.add(Conv1D(128, 3, activation='tanh', padding='same'))
+    model.add(Conv1D(128, 3, activation='relu', padding='same'))
     model.add(MaxPooling1D(3))
     model.add(Dropout(0.2))
 
-    model.add(Conv1D(128, 3, activation='tanh', padding='same'))
+    model.add(Conv1D(128, 3, activation='relu', padding='same'))
     model.add(MaxPooling1D(3))
     model.add(Dropout(0.2))
 
