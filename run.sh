0476f9cbaf2fd6dfeccd9c3ed4664624be13ab86
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-06-27 15:27:32 +0200

    uncomment run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-# conda create --name remlapy310 python=3.10
-# conda activate remlapy310
+conda create --name remlapy310 python=3.10
+conda activate remlapy310
 pip install poetry
 poetry install
-# mkdir datasets
+mkdir datasets
 dvc repro
