8d604630858bacac5f8c7b25d97401c6480d45d1
Shantnav Agarwal <shantnavagarwal@gmail.com>
2024-06-27 12:44:39 +0200

    new model trained

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-conda create --name remlapy310 python=3.10
-conda activate remlapy310
-mkdir datasets
+# conda create --name remlapy310 python=3.10
+# conda activate remlapy310
+pip install poetry
+poetry install
+# mkdir datasets
 dvc repro
